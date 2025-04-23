57059607e8bc094a42b6f3e844595a83fdb46752
MOHANAPRIYA-0106 <131444681+MOHANAPRIYA-0106@users.noreply.github.com>
2025-04-23 12:18:00 +0530

    Day01  uploaded

---
+++ Day01/file2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#define square(x) x*x
+
+using namespace std;
+int main()
+{
+    cout<<square(10+2)<<endl;
+}
---
+++ Day01/increment.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int x = 10;
+
+	int y = x++;
+
+	int z = ++x;
+
+	 std::cout << x ;
+
+	 std::cout << y ;
+
+	 std::cout << z;
+
+	return 0;
+
+}
