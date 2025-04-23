a8ab2b4d185769dcbc5f9b6eeb45445080c0f34b
MOHANAPRIYA-0106 <131444681+MOHANAPRIYA-0106@users.noreply.github.com>
2025-04-23 23:43:54 +0530

    Day04 uploaded

---
+++ Day04/ifelse/ascii.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char letter = 'A';
+	string word = "hello";
+	cout<< "Enter a letter: "<<letter<<"ASCII value: "<<int(letter)<<endl;
+
+	cout<< "Enter a word: "<<word<<"ASCII value: "<<int(word[0])<<endl;
+	cout<< "Enter a word: "<<word<<"ASCII value: "<<int(word[1])<<endl;
+	cout<< "Enter a word: "<<word<<"ASCII value: "<<int(word[2])<<endl;
+	cout<< "Enter a word: "<<word<<"ASCII value: "<<int(word[3])<<endl;
+	cout << "Word" << endl;
+
+
+
+
+}
+
